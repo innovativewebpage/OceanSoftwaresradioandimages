2808603a581f69fbcc8acc89ecd01392e2bcd06f
Omar Rizwan <innovativewebpage@gmail.com>
2025-03-04 17:42:18 +0530

    little changes

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-//hi omar
+//hi omar how are you
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
